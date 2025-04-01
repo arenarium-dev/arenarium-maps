980ef3f9c7eafc43b9457b3cbc4309c12b5a2bb8
Marko Smiljanic <marko.smilja@gmail.com>
2025-04-01 14:38:01 +0200

    Work on test

---
+++ packages/svelte-kit-test/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import { mountMap } from '@arenarium/maps';
+	import '@arenarium/maps/dist/style.css';
+
+	onMount(() => {
+		mountMap('map');
+	});
+</script>
+
+<div id="map"></div>
--- packages/svelte-kit-test/src/routes/component/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import { Map } from '@arenarium/maps';
-</script>
-
-<Map />
