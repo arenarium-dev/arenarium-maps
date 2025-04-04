f9cd3a61cb63fb8f83aeceb0b19fe5ad6b016021
Marko Smiljanic <marko.smilja@gmail.com>
2025-04-04 15:50:22 +0200

    Removed hono api

--- packages/src/api/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
-import { defineConfig } from 'vite'
-import ssrHotReload from 'vite-plugin-ssr-hot-reload'
-
-export default defineConfig({
-  plugins: [ssrHotReload(), cloudflare()]
-})
