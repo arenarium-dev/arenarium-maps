9633191958aa797c404630fbe9f2f79b67aaf63e
Marko Smiljanic <marko.smilja@gmail.com>
2025-05-18 15:42:34 +0200

    Added error logging

--- packages/src/web/src/routes/(layout)/api/public/[version]/popup/states/+server.ts
+++ packages/src/web/src/routes/(layout)/api/public/[version]/popup/states/+server.ts
@@ -1,8 +1,6 @@
-import { error } from '@sveltejs/kit';
-
 import * as schema from '$lib/server/database/schema';
 import { getDb } from '$lib/server/database/client';
-import { USAGE_MAX_ITEMS, USAGE_MAX_TIMESPAN } from '$lib/shared/constants';
+import { DISCORD_WEBHOOK_URL, USAGE_MAX_ITEMS, USAGE_MAX_TIMESPAN } from '$lib/shared/constants';
 
 import { API_KEY_FREE_KEY } from '$env/static/private';
 
@@ -24,15 +22,6 @@ export const OPTIONS: RequestHandler = () => {
 };
 
 export const POST: RequestHandler = async (event) => {
-	const request: Popup.StatesRequest = await event.request.json();
-	if (!request) error(400, 'Invalid request body');
-
-	const data = request.data;
-	if (!data) error(400, 'Missing data');
-
-	const key = request.key;
-	if (!key) error(400, 'Missing API key');
-
 	// Create the headers
 	const headers = {
 		'Access-Control-Allow-Origin': '*',
@@ -40,47 +29,75 @@ export const POST: RequestHandler = async (event) => {
 		'Access-Control-Allow-Headers': 'Content-Type'
 	};
 
-	// Check if the API key is free
-	if (key != API_KEY_FREE_KEY) {
-		const db = getDb(event.platform?.env.DB);
-		const dbApiKey = await db.query.apiKeys.findFirst({
-			where: (k, { and, eq }) => and(eq(k.key, key), eq(k.active, true))
-		});
-		if (!dbApiKey) error(404, 'API key not found');
+	const response = (status: number, message: string) => new Response(null, { headers, status, statusText: message });
+	const result = (data: any) => new Response(JSON.stringify(data), { headers, status: 200 });
 
-		// Chech the api key rate limit
-		if (dbApiKey.unlimited != true) {
-			// Check is the request itself is larger than the limit
-			if (data.length > USAGE_MAX_ITEMS) {
-				return new Response(null, { headers, status: 429, statusText: 'Request too large!' });
+	try {
+		const request: Popup.StatesRequest = await event.request.json();
+		if (!request) return response(400, 'Missing request');
+
+		const data = request.data;
+		if (!data) return response(400, 'Missing data');
+
+		const key = request.key;
+		if (!key) return response(400, 'Missing API key');
+
+		// Check if the API key is free
+		if (key != API_KEY_FREE_KEY) {
+			const db = getDb(event.platform?.env.DB);
+			const dbApiKey = await db.query.apiKeys.findFirst({
+				where: (k, { and, eq }) => and(eq(k.key, key), eq(k.active, true))
+			});
+			if (!dbApiKey) return response(404, 'API key not found');
+
+			// Chech the api key rate limit
+			if (dbApiKey.unlimited != true) {
+				// Check is the request itself is larger than the limit
+				if (data.length > USAGE_MAX_ITEMS) {
+					return new Response(null, { headers, status: 429, statusText: 'Request too large!' });
+				}
+
+				// Get the total usage from the last second
+				const dbUsageSumResult = await db
+					.select({ sum: sum(schema.apiKeyUsages.count) })
+					.from(schema.apiKeyUsages)
+					.where(and(eq(schema.apiKeyUsages.keyId, dbApiKey.id), gt(schema.apiKeyUsages.date, new Date(Date.now() - USAGE_MAX_TIMESPAN))));
+
+				const dbUsageSum = Number.parseInt(dbUsageSumResult.at(0)?.sum ?? '0');
+
+				// Check if the total sum of request is larger than the limit
+				if (dbUsageSum + data.length > USAGE_MAX_ITEMS) {
+					return new Response(null, { headers, status: 429, statusText: 'Too many large requests!' });
+				}
+
+				// Update usage
+				const dbUsage: schema.DbApiKeyUsageInsert = {
+					id: crypto.randomUUID(),
+					keyId: dbApiKey.id,
+					count: data.length
+				};
+				await db.insert(schema.apiKeyUsages).values([dbUsage]);
 			}
-
-			// Get the total usage from the last second
-			const dbUsageSumResult = await db
-				.select({ sum: sum(schema.apiKeyUsages.count) })
-				.from(schema.apiKeyUsages)
-				.where(and(eq(schema.apiKeyUsages.keyId, dbApiKey.id), gt(schema.apiKeyUsages.date, new Date(Date.now() - USAGE_MAX_TIMESPAN))));
-
-			const dbUsageSum = Number.parseInt(dbUsageSumResult.at(0)?.sum ?? '0');
-
-			// Check if the total sum of request is larger than the limit
-			if (dbUsageSum + data.length > USAGE_MAX_ITEMS) {
-				return new Response(null, { headers, status: 429, statusText: 'Too many large requests!' });
-			}
-
-			// Update usage
-			const dbUsage: schema.DbApiKeyUsageInsert = {
-				id: crypto.randomUUID(),
-				keyId: dbApiKey.id,
-				count: data.length
-			};
-			await db.insert(schema.apiKeyUsages).values([dbUsage]);
 		}
+
+		// Get the states
+		const states = getStates(data);
+
+		// Return the response
+		return result(states);
+	} catch (error: any) {
+		console.error(error);
+
+		// Log to discord
+		await event.fetch(DISCORD_WEBHOOK_URL, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				username: `[Error] ${error.message}`,
+				content: '```' + `${JSON.stringify(error, null, 2)}` + '```'
+			})
+		});
+
+		return response(500, 'Internal server error');
 	}
-
-	// Get the states
-	const states = getStates(data);
-
-	// Return the response
-	return new Response(JSON.stringify(states), { headers, status: 200 });
 };
