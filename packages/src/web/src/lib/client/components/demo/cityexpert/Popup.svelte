55c05489372d413bb3266c8acc34f31419583591
Marko Smiljanic <marko.smilja@gmail.com>
2025-06-05 12:50:30 +0200

    Bug fixing

--- packages/src/web/src/lib/client/components/demo/cityexpert/Popup.svelte
+++ packages/src/web/src/lib/client/components/demo/cityexpert/Popup.svelte
@@ -87,17 +87,13 @@
 {/if}
 
 <style lang="less">
-	@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
-	:root {
+	.popup {
 		--primary: #df2d43;
 		--font-regular: 'Poppins', 'Montserrat', sans-serif;
 		--font-secondary: 'Rubik', sans-serif;
 		--font-numerical: 'Montserrat', sans-serif;
 		--gray: rgb(244 245 246);
-	}
 
-	.popup {
 		position: absolute;
 		display: flex;
 		flex-direction: column;
